efdc3fa5eaa31ef9fe4dce94799da2a99266805e
WyrsPatrHSLU <patrik.wyrsch@stud.hslu.ch>
2020-09-23 10:25:37 +0200

    ifelse-test

---
+++ 30_Exercises/SW02/ifelsetest.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main ( int argc, char* argv[] ) {
+
+	int testValue =2;
+	int limitValue =3;
+	
+	if (testValue >=limitValue) {
+		printf("%d is bigger than or equal to %d\n",
+			testValue, limitValue);
+	}
+	else {
+		printf("%d is less than %d\n", testValue, limitValue);
+	}
+	return 0;
+}
